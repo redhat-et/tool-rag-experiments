bdda12fb50e7b26d5be5a36d4373c4f309d83a98
Ilya Kolchinsky <58424190+ilya-kolchinsky@users.noreply.github.com>
2025-10-23 13:56:29 +0200

    Fixed the way the framework augments the baseline execution with irrelevant tools. (#33)

--- evaluator/algorithms/baseline_algorithm.py
+++ evaluator/algorithms/baseline_algorithm.py
@@ -1,3 +1,4 @@
+import random
 from typing import Dict, List, Any
 
 from langchain_core.language_models import BaseChatModel
@@ -8,10 +9,17 @@ from evaluator.components.data_provider import QuerySpecification
 from evaluator.config.schema import ModelConfig
 from evaluator.utils.module_extractor import register_algorithm
 from evaluator.interfaces.algorithm import Algorithm, AlgoResponse
+from evaluator.utils.utils import log_verbose
 
 
 @register_algorithm("baseline_algorithm")
 class BaselineAlgorithm(Algorithm):
+    """
+    Optional configurable settings (to be provided in the 'settings' dictionary):
+
+    - available_tools_per_query: this number specifies how many tools the model will see per query. Set this
+      parameter to None to disable adding irrelevant tools.
+    """
     def __init__(self, settings: Dict, model_config: List[ModelConfig], label: str = None):
         super().__init__(settings, model_config, label)
         self.all_tools = None
@@ -20,19 +28,22 @@ class BaselineAlgorithm(Algorithm):
         super().set_up(model, tools)
         self.all_tools = tools
 
-    @staticmethod
-    def _is_tool_relevant(query_spec: QuerySpecification, tool_name: str) -> bool:
-        if tool_name in query_spec.golden_tools:
-            return True
-        if not query_spec.additional_tools:
-            return False
-        return tool_name in query_spec.additional_tools
-
     def _filter_relevant_tools(self, query_spec: QuerySpecification) -> List[BaseTool]:
         filtered_tools = []
         for tool in self.all_tools:
-            if self._is_tool_relevant(query_spec, tool.name):
+            if tool.name in query_spec.golden_tools:
                 filtered_tools.append(tool)
+
+        if not self._settings["available_tools_per_query"]:
+            # tool list augmentation disabled - only return the 'golden' tools
+            return filtered_tools
+
+        additional_tools_num = self._settings["available_tools_per_query"] - len(filtered_tools)
+        remaining_tools = [t for t in self.all_tools if t not in filtered_tools]
+        additional_tools = random.sample(remaining_tools, additional_tools_num)
+        filtered_tools.extend(additional_tools)
+        random.shuffle(filtered_tools)
+
         return filtered_tools
 
     async def process_query(self, query_spec: QuerySpecification) -> AlgoResponse:
@@ -40,6 +51,7 @@ class BaselineAlgorithm(Algorithm):
             raise RuntimeError("process_query called before set_up")
 
         tools = self._filter_relevant_tools(query_spec)
+        log_verbose(f"The following tools were loaded for query {query_spec.id}:\n{[t.name for t in tools]}")
         agent = create_react_agent(self._model, tools)
         return await self._invoke_agent_on_query(agent, query_spec.query), None
 
